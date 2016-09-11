1023c66f799b0947e2a2ed844cbcc28857a57d67
Nandhini <nandhini.devi1998@gmail.com>
2016-09-11 09:21:29 +0530

    Feature ADD: modified models to include relationships (many to one and one to many)

---
+++ app/comment.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class comment extends model
+{
+	public $timestamps=false;
+	public function thread(){
+	return this->belongsTo('website_thread');
+	}
+}
---
+++ database/seeds/commentsTableSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class commentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    
+}
